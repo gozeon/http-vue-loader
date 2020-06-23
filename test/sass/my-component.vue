e4c8b99e4d13e26def376aa2183dd67aea7cd617
goze <goze.qiu@gmail.com>
2020-06-23 17:56:11 +0800

    test lang

---
+++ test/sass/my-component.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="hello">Hello {{who}}</div>
+</template>
+
+<script>
+module.exports = {
+    data: function() {
+        return {
+            who: 'world'
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+$primary-color: #333;
+$color: #ffffff;
+
+.hello {
+    background-color: $primary-color;
+    color: $color;
+}
+</style>
